d5f779a85ea00a23a4b7d0560b4f7f095d53792d
bagautdinov23 <bagautdinov23@gmail.com>
2016-10-30 23:25:54 +0300

    Домашняя работа. Осталось дописать 2 метода для студента

---
+++ University.java
@@ -0,0 +1,14 @@
+public class University {
+	private String name;
+
+	University() {
+	}
+
+	public void setName(String name) {
+	this.name = name;
+	}
+
+	public String getName() {
+	return name;
+	}
+}
